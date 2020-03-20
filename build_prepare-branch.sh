6ca767b5397649412d470f87bb325b9031368857
Toussaint Nicolas <nicolas1.toussaint@orange.com>
2020-03-20 15:04:01 +0100

    make script output more visible in gitlab logs

--- build_prepare-branch.sh
+++ build_prepare-branch.sh
@@ -10,21 +10,28 @@ f_fatal() {
     echo "$@"
     exit 1
 }
+
+f_log() {
+    echo "<<< $*"
+}
+
 docker_deploy="docker-deploy"
 
 branch="$1"
 [ -n "$branch" ] || f_fatal "Arg. missing"
 
-echo "Processing: '$branch'"
+f_log "-----------------------------------------------"
+f_log "$(basename $0)"
+f_log  "Processing: '$branch'"
 if echo "$branch" | grep -q ".*/$docker_deploy"
 then
     # Classic 
     branch_root=$(echo "$branch" | sed 's!/.*$!!')
-    echo "Perform Octopus merge for '$branch_root'"
+    f_log "Perform Octopus merge for '$branch_root'"
     git branch --list -r "origin/$branch_root/feat/*"
     git branch --list -r "origin/$branch_root/feat/*" | \
 		xargs git merge --verbose --no-commit
 else
-    echo "No Octopus merge required for '$branch_root'"
+    f_log "No Octopus merge required for '$branch_root'"
 fi
-
+f_log "-----------------------------------------------"
